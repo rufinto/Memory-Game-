c0bf3f8f93ba467d17e0d7e44605902b1f659fa8
Toukap Nguepteo Joan-Rufin <joan-rufin.toukap-nguepteo@student-cs.fr>
2023-11-20 16:58:57 +0100

    mise a jour

--- choix.py
+++ choix.py
@@ -12,33 +12,48 @@ def enregistrer_nom_joueur():
 
 # Création de la fenêtre principale
 fenetre = tk.Tk()
+
+width = 500
+height = 600
+
+screen_x = fenetre.winfo_screenwidth()    
+screen_y = fenetre.winfo_screenheight()
+window_x = width
+window_y = height
+
+posX = (screen_x // 2) - (window_x // 2)
+posY = (screen_y // 2) - (window_y // 2)
+geo = "{}x{}+{}+{}".format(window_x, window_y, posX, posY)
+
+fenetre.minsize(width,height)
 fenetre.title("Jeu")
+fenetre.geometry(geo)
 
 # Zone de saisie pour le nom du joueur
 nom_joueur_label = tk.Label(fenetre, text="Nom du joueur:")
-nom_joueur_label.pack()
-nom_joueur_entry = tk.Entry(fenetre)
-nom_joueur_entry.pack()
+nom_joueur_label.grid(row=15, column=4)
+nom_joueur_entry = tk.Entry(fenetre, width=50)
+nom_joueur_entry.grid(row=25, column = 4)
 
 # Boutons pour les thèmes
 theme1_button = tk.Button(fenetre, text="Thème 1", command=lambda: choisir_theme(1))
-theme1_button.pack(side=tk.LEFT)
+theme1_button.grid(row = 100, column= 3)
 theme2_button = tk.Button(fenetre, text="Thème 2", command=lambda: choisir_theme(2))
-theme2_button.pack(side=tk.LEFT)
+theme2_button.grid(row = 200, column = 3)
 theme3_button = tk.Button(fenetre, text="Thème 3", command=lambda: choisir_theme(3))
-theme3_button.pack(side=tk.LEFT)
+theme3_button.grid(row = 300, column = 3)
 
 # Boutons pour les niveaux
 niveau1_button = tk.Button(fenetre, text="Niveau 1", command=lambda: choisir_niveau(1))
-niveau1_button.pack(side=tk.LEFT)
+niveau1_button.grid(row= 100, column = 5)
 niveau2_button = tk.Button(fenetre, text="Niveau 2", command=lambda: choisir_niveau(2))
-niveau2_button.pack(side=tk.LEFT)
+niveau2_button.grid(row=200, column = 5)
 niveau3_button = tk.Button(fenetre, text="Niveau 3", command=lambda: choisir_niveau(3))
-niveau3_button.pack(side=tk.LEFT)
+niveau3_button.grid(row = 300, column = 5)
 
 # Bouton pour enregistrer le nom du joueur
 enregistrer_button = tk.Button(fenetre, text="Enregistrer", command=enregistrer_nom_joueur)
-enregistrer_button.pack()
+enregistrer_button.grid(row = 500, column = 4)
 
 # Lancement de la boucle principale
 fenetre.mainloop()
