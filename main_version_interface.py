8a0857e9fb833b9929b2eb381d4f446f091c1912
Ouhmidou Sarra <sarra.ouhmidou@student-cs.fr>
2023-11-22 20:21:53 +0100

    v3 interface

--- main_version_interface.py
+++ main_version_interface.py
@@ -1,13 +1,13 @@
 from cards import *
 from classes import *
-from interface2 import *
+from interface_complet import *
 
 def main():
     create_all_cards()
     associate_all_pairs(1) #on fait que pour le theme 1 et 2
     associate_all_pairs(2)
     associate_all_pairs(3)
-    open_pseudo_window()
+    #open_pseudo_window()
 
 #while(not game.is_finished()): #tant que le jeu n'est pas terminée on enregistre les choix du joueur 
 main()
